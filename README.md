45bc6d224dbb6f0085b84605dbf2f9b9784ac1f2
andreabduque <abd@cin.ufpe.br>
2017-06-17 23:27:05 -0300

    Update readme

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # GAFE
 Implementation of Genetic Algorithm for Functional Expansion (GAFE),
 useful in data preprocessing tasks. GAFE chooses an optimal functional expansion and size expansion pair and expands data attributes, enhancing classifier performance.
+
 This [paper](http://www.sciencedirect.com/science/article/pii/S0167865517300351) describes the technique.
 
+GAFE depends on the python packages Numpy, Pandas, Scikit-Learn and DEAP.
+
+Tutorial notebook [here](https://github.com/andreabduque/GAFE/blob/master/notebooks/GAFE%20tutorial.ipynb).
+For using notebooks, install Jupyter Notebook.
--- main.py
+++ main.py
@@ -6,43 +6,25 @@ import pandas as pd
 import ga
 
 def testIris():
-    functionalExp = FE()
-    iris = pd.read_csv("iris.data", sep=",")
+
+    iris = pd.read_csv("data/iris.data", sep=",")
     irisAtts = iris.drop("class", 1)
     target = iris["class"]
 
     #Scale to [0,1] before expanding
     scaledIris = MinMaxScaler().fit_transform(irisAtts)
-    data_pw_5 = functionalExp.expandMatrix(scaledIris, [(1,5) for x in range(4)])
-    # data_bo_2 = functionalExp.expandMatrix(scaledIris, [(3,2) for x in range(4)])
-    # data_lu_4 = functionalExp.expandMatrix(scaledIris, [(7,4) for x in range(4)])
 
-    model = Classifier('knn', target, folds=10, jobs=6)
-    gafe = ga.GAFE(model, scaledIris, target, scaled=True)
-    print("original 1nn " + str(model.getAccuracy(irisAtts)))
-    print("expansao 1nn " + str(model.getAccuracy(data_pw_5)))
-    avg, bestPair = gafe.runGAFE(n_population=21, n_iter=1, verbose=True)
-    print("gafe 1nn " +  str(avg))
-    print("best pair ")
-    print(bestPair)
+    bestSingleMatch = {'knn': [(1,5) for x in range(4)], 'cart': [(3,2) for x in range(4)], 'svm': [(7,4) for x in range(4)]}
+    functionalExp = FE()
 
-    # print("original cart " + str(model.cart(irisAtts)))
-    # print("expansao cart " + str(model.cart(data_bo_2)))
-    #
-    # print("original svm " + str(model.svm(irisAtts)))
-    # print("expansao svm " + str(model.svm(data_lu_4)))
-
-    # print(functionalExp.expandMatrix(irisAtts.as_matrix(), [(1,1), (1,1), (1,1), (1,1)]))
-
-
-# def testFib():
-#     functionalExp = FE()
-#     att = np.array([1,2,3])
-#
-#     for fe in ["PW", "TR", "BO", "FI", "CH1", "CH2", "LU"]:
-#         for es in range(1,8):
-#             print("func exp " + fe + " size " + str(es))
-#             print(functionalExp.eval(fe, es, att))
+    for cl in ['knn', 'cart', 'svm']:
+        model = Classifier(cl, target, folds=10, jobs=6)
+        print("original accuracy " + cl + " " + str(model.getAccuracy(irisAtts)))
+        expandedData = functionalExp.expandMatrix(scaledIris, bestSingleMatch[cl])
+        print("single match expansion accuracy " + cl + " " + str(model.getAccuracy(expandedData)))
+        gafe = ga.GAFE(model, scaledIris, target, scaled=True)
+        avg, bestPair = gafe.runGAFE(n_population=21, n_iter=1, verbose=True)
+        print("gafe " + cl + " " + str(avg) )
 
 def main():
     # testFib()
